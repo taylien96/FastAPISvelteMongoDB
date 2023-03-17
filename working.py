d7d8bde7c1e7106df160670d1459a91797e0c304
Taylor Alan <tay10alan60@gmail.com>
2023-03-17 14:09:46 -0700

    pipfile ect

--- working.py
+++ working.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query, HTTPException, status
 from typing import Optional
 from pydantic import BaseModel
 
@@ -8,7 +8,10 @@ class Item(BaseModel):
     name: str
     price: float
     brand: Optional[str] = None
-
+class UpdateItem(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    brand: Optional[str] = None
 inventory = {
     
 }
@@ -26,7 +29,7 @@ def get_item(name: Optional[str] = None):
     for item_id in inventory:
         if inventory[item_id].name == name:
             return inventory[item_id]
-    return {"data" : "no exist"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
 @app.post("/create-item/{item_id}")
 def create_item(item_id: int, item: Item):
@@ -37,9 +40,18 @@ def create_item(item_id: int, item: Item):
     #is the same as
     inventory[item_id] = item
     return inventory[item_id]
-@app.put("update-item/{item_id}")
-def update_item(item_id: int, item: Item):
+@app.put("/update-item/{item_id}")
+def update_item(item_id: int, item: UpdateItem):
     if item_id not in inventory:
         return {"Error":"Item id does not exist"}
     inventory[item_id] = item
+    #if you dont want to change null values go through individuallly like
+    #item.name ! = none
+    #inventory[item_id].name = name  
     return inventory[item_id]
+@app.delete("/delete-item")
+def delete_item(item_id: int = Query(..., description="The id of the item deleted")):
+    if item_id not in inventory:
+        return {"error" : "item aint ther dawg"}
+    del inventory[item_id]
+    return{"success" : "zappppped"}
